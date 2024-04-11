c5acd3fc510cb2021b6f479ed297fbe3e23d95ea
kimzz14 <kimzz1004@gmail.com>
2024-04-11 13:23:45 +0900

    formating

--- pipe/run_pbmm2.sh
+++ pipe/run_pbmm2.sh
@@ -6,12 +6,14 @@ readDir=/test
 
 pbmm2 align \
 	--preset CCS \
+	--log-level INFO \
 	reference/ref.fa \
 	${readDir}/${readID}.fastq.gz \
 	result/${readID}.pbmm2.bam \
 	1> result/${readID}.pbmm2.bam.log \
 	2> result/${readID}.pbmm2.bam.err
 
-samtools flagstat result/${readID}.pbmm2.bam \
-1> result/${readID}.pbmm2.bam.flagstat \
-2> result/${readID}.pbmm2.bam.flagstat.log
+samtools flagstat \
+    result/${readID}.pbmm2.bam \
+    1> result/${readID}.pbmm2.bam.flagstat \
+    2> result/${readID}.pbmm2.bam.flagstat.log
